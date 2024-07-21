28527e6a02dea2fcc921f8896442aff94e5bdfaa
dav-dmdo <david.davila@correo.unimet.edu.ve>
2024-07-21 19:48:44 -0400

    First cimmit

---
+++ CursoBackendSln/CursoBackend/Controllers/OperationController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        [HttpPost]
+        public decimal Sub(
+            Numbers numbers, 
+            [FromHeader] string Host, 
+            [FromHeader(Name = "Content-Length")] string ContentLength,
+            [FromHeader(Name = "X-Some")] string Some)
+        {
+            Console.WriteLine(Host);
+            Console.WriteLine(ContentLength);
+            Console.WriteLine(Some);
+            return numbers.A - numbers.B;
+        }
+
+        [HttpPut]
+        public decimal Mul(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        [HttpDelete]   
+        public decimal Div(decimal a, decimal b)
+        {
+            return a / b;
+        }
+    }
+
+    public class Numbers
+    {
+        public decimal A { get; set; }
+        public decimal B { get; set; }
+    }
+
+}
---
+++ CursoBackendSln/CursoBackend/Controllers/PeopleController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet("all")]
+        public List<People> GetPeople() => Repository.People;
+
+        [HttpGet("{id}")]
+        public People Get(int id) => Repository.People.First(p => p.Id == id);
+
+        [HttpGet("search/{name}")]
+        public People Get(string name) => 
+            ;
+    }
+
+    public class Repository
+    {
+        public static List<People> People = new List<People>
+        {
+            new People(){Id = 1, Name = "John", Birthdate = new DateTime(1990, 1, 1)},
+            new People(){Id = 2, Name = "Mary", Birthdate = new DateTime(1991, 1, 1)},
+            new People(){Id = 3, Name = "Peter", Birthdate = new DateTime(1992, 1, 1)},
+            new People(){Id = 4, Name = "Paul", Birthdate = new DateTime(1993, 1, 1)},
+        };
+
+    }
+
+
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+
+
+}
