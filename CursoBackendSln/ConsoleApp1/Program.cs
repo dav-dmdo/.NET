37a266347d962fd32e8296869868bdb2f15c4a21
dav-dmdo <david.davila@correo.unimet.edu.ve>
2024-08-07 00:04:17 -0400

    Running CallAPI.cs test 3

---
+++ CursoBackendSln/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+string url = "https://localhost:7062/api/people/all";
+
+using (HttpClient client = new HttpClient())
+{
+    
+    HttpResponseMessage response = await client.GetAsync(url);
+
+    if (response.IsSuccessStatusCode)
+    {
+        string responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine("API Response: " + responseBody);
+    }
+    else
+    {
+        Console.WriteLine("Error: " + response.StatusCode);
+    }
+}
