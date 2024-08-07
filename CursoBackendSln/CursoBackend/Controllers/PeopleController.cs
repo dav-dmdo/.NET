2f0d853d38cd49b4d127791b170802453644cad5
dav-dmdo <david.davila@correo.unimet.edu.ve>
2024-08-06 23:26:45 -0400

    nullable added

--- CursoBackendSln/CursoBackend/Controllers/PeopleController.cs
+++ CursoBackendSln/CursoBackend/Controllers/PeopleController.cs
@@ -66,7 +66,7 @@ namespace CursoBackend.Controllers
     public class People
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public DateTime Birthdate { get; set; }
     }
 
