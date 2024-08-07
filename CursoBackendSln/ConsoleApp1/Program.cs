963fd8b8c6eead900c894f649fb76f45c9e2eaf3
dav-dmdo <david.davila@correo.unimet.edu.ve>
2024-08-07 00:07:35 -0400

    Running CallAPI.cs test 4

--- CursoBackendSln/ConsoleApp1/Program.cs
+++ CursoBackendSln/ConsoleApp1/Program.cs
@@ -5,19 +5,20 @@ using System.Net.Http;
 using System.Threading.Tasks;
 
 string url = "https://localhost:7062/api/people/all";
+Console.WriteLine(url);
 
-using (HttpClient client = new HttpClient())
-{
+// using (HttpClient client = new HttpClient())
+// {
     
-    HttpResponseMessage response = await client.GetAsync(url);
+//     HttpResponseMessage response = await client.GetAsync(url);
 
-    if (response.IsSuccessStatusCode)
-    {
-        string responseBody = await response.Content.ReadAsStringAsync();
-        Console.WriteLine("API Response: " + responseBody);
-    }
-    else
-    {
-        Console.WriteLine("Error: " + response.StatusCode);
-    }
-}
+//     if (response.IsSuccessStatusCode)
+//     {
+//         string responseBody = await response.Content.ReadAsStringAsync();
+//         Console.WriteLine("API Response: " + responseBody);
+//     }
+//     else
+//     {
+//         Console.WriteLine("Error: " + response.StatusCode);
+//     }
+// }
